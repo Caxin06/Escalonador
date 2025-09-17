e166b1f29b9b07439ff32cec1553e1f1c9253ce4
Caxin06 <absalaoresplandes@gmail.com>
2025-09-17 00:46:05 -0300

    Adicionando-metodos-da-lista

--- src/ListaDeProcessos.java
+++ src/ListaDeProcessos.java
@@ -9,7 +9,33 @@ public class ListaDeProcessos {
         this.tamanho = 0;
     }
 
+    public void AdcionarFim(Processo processo ) {
+        Node novoNo = new Node( processo );
 
+        if (this.cabeça == null) {
+            this.cabeça = novoNo;
+            this.cauda = novoNo;
+            tamanho++;
+        }
+        cauda.proximo = novoNo;
+        novoNo.anterior = cauda;
+        cauda = novoNo;
+    }
+    public Processo removerDoInicio() {
+        if (this.cabeça == null) {
+            return null;
+        }
+        Node removido = cabeça;
+        if (cabeça == cauda) {
+            cabeça = null;
+            cauda = null;
+        } else {
+            cabeça = cabeça.proximo;
+            cabeça.anterior = null;
+            removido.proximo = null;
+        }
+        return removido.processo;
+    }
 
 }
 
